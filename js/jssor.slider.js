89f63ea4b60f24fda2894e05bf32b66edd2754bf
jssor <zazanaire@live.com>
2014-10-10 06:50:52 +0800

    deal with chrome 38

--- js/jssor.slider.js
+++ js/jssor.slider.js
@@ -1233,6 +1233,9 @@ new function () {
                         var interPosition = newPosition / _Duration;
                         if ($Jssor$.$IsBrowserChrome() || $Jssor$.$IsBrowserFireFox()) {
                             Math.round(interPosition * 16 / _Duration) / 16 * _Duration;
+
+                            if ($Jssor$.$BrowserVersion() < 38)
+                                interPosition = parseFloat(interPosition.toFixed(4));
                         }
                         toPosition = _Options.$SlideEasing(interPosition) * (_ToPosition - _FromPosition) + _FromPosition;
                     }
@@ -1930,8 +1933,14 @@ new function () {
             var x = _StepLengthX * position * (orientation & 1);
             var y = _StepLengthY * position * ((orientation >> 1) & 1);
 
-            x = Math.round(x);
-            y = Math.round(y);
+            if ($Jssor$.$IsBrowserChrome() && $Jssor$.$BrowserVersion() < 38) {
+                x = x.toFixed(3);
+                y = y.toFixed(3);
+            }
+            else {
+                x = Math.round(x);
+                y = Math.round(y);
+            }
 
             if ($Jssor$.$IsBrowserIE() && $Jssor$.$BrowserVersion() >= 10 && $Jssor$.$BrowserVersion() < 11) {
                 elmt.style.msTransform = "translate(" + x + "px, " + y + "px)";
