5e783184d3c6a9abf2082603ec212819ba23a64c
jssor <zazanaire@live.com>
2016-01-19 18:00:33 +0800

    improve 3d animation, update demos

--- examples-no-jquery/slider-with-caption.compress.bat
+++
@@ -1,1 +0,0 @@
-"..\tool\Jssor.Compress.exe" -HTML:Js -HTML:Css -HTML:Comment -HTML:Blank -JSSORONLY -AWRAP -OVERWRITE -ENCOUT:UTF-8 "slider-with-caption.source.html" -OUT "slider-with-caption.html"
